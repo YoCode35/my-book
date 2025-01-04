f82e475f6a332db9970aeaebef8f8e406c7eb3b2
YoCode35 <ygregoire35@gmail.com>
2025-01-04 04:29:02 +0100

    Réintégration du comportement de fermeture du menu au clic sur un lien et ajustement du background

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,13 +18,13 @@ export default function Navbar({ menuOpen, setMenuOpen }: NavbarProps) {
   return (
     <>
       <nav className="absolute top-3 left-0 w-full flex justify-center z-10">
-      <div className="w-[95%] sm:w-[90%] md:w-[85%] lg:max-w-[900px] px-2 py-2 bg-[rgba(1,22,39,0.6)] rounded-full border border-gray-800 flex justify-between items-center">
+        <div className="w-[95%] sm:w-[90%] md:w-[85%] lg:max-w-[900px] px-2 py-2 bg-[rgba(1,22,39,0.6)] rounded-full border border-gray-800 flex justify-between items-center">
           <div className="text-white font-orbitron text-[8px] sm:text-[10px] md:text-[12px] lg:text-[14px] ml-2 sm:ml-4 md:ml-6 lg:ml-8">
             Yoann GREGOIRE
           </div>
 
           {/* Icône Sandwich (Hamburger) sur les petits écrans */}
-          <div className="lg:hidden flex items-center" style={{ transform: 'translateX(-10px)' }}> {/* Ajuste ici le -5px */}
+          <div className="lg:hidden flex items-center" style={{ transform: 'translateX(-10px)' }}>
             <button onClick={() => setMenuOpen(!menuOpen)} className="text-white">
               <FiMenu size={30} />
             </button>
@@ -32,7 +32,7 @@ export default function Navbar({ menuOpen, setMenuOpen }: NavbarProps) {
 
           {/* Menu Desktop */}
           <ul className="hidden lg:flex space-x-4">
-            {[
+            {[ 
               { href: "/", label: "Accueil" },
               { href: "/about", label: "À propos" },
               { href: "/portfolio", label: "Portfolio" },
@@ -40,9 +40,7 @@ export default function Navbar({ menuOpen, setMenuOpen }: NavbarProps) {
               <li key={href}>
                 <Link
                   href={href}
-                  className={`px-6 py-2 text-navLinkInactive hover:text-navLinkHover ${isActive(
-                    href
-                  ) === "text-navText"
+                  className={`px-6 py-2 text-navLinkInactive hover:text-navLinkHover ${isActive(href) === "text-navText"
                     ? "border-b-2 border-yellow-500"
                     : ""} ${isActive(href)}`}
                 >
@@ -83,13 +81,17 @@ export default function Navbar({ menuOpen, setMenuOpen }: NavbarProps) {
             <Link
               key={href}
               href={href}
+              onClick={() => setMenuOpen(false)} // Fermeture du menu au clic
               className={`block text-center px-4 py-2 rounded-md text-navLinkInactive hover:text-navLinkHover ${isActive(href)}`}
             >
               {label}
             </Link>
           ))}
           <Link href="/contact">
-            <button className="block w-[150px] mx-auto px-4 py-2 mt-6 rounded-full bg-[rgba(128,0,128,0.3)] text-white hover:bg-[rgba(128,0,128,0.6)] focus:outline-none focus:ring-2 focus:ring-purple-300">
+            <button
+              onClick={() => setMenuOpen(false)} // Fermeture du menu lorsque "Me contacter" est cliqué
+              className="block w-[150px] mx-auto px-4 py-2 mt-6 rounded-full bg-[rgba(128,0,128,0.3)] text-white hover:bg-[rgba(128,0,128,0.6)] focus:outline-none focus:ring-2 focus:ring-purple-300"
+            >
               Me contacter
             </button>
           </Link>
