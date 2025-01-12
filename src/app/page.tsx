638aa220e9f26559010a8ea6572cf352697a8472
YoCode35 <ygregoire35@gmail.com>
2025-01-12 13:42:17 +0100

    New Margin Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -132,113 +132,68 @@ export default function Home() {
       {/* SEPARATEUR */}
       <div className="my-80 sm:my-64 md:my-72 lg:my-64 xl:my-32"></div>
 
-      {/* Skills Section */}
-      <div className="">
+      {/* About Section */}
+      <div id="skills" className=" lg:flex                                      
+                                      w-full h-[600px] sm:h-[550px] md:h-[550px] lg:h-0px]
+                                      md:pt-0"
+      >
 
-      <div id="skills" className="flex flex-col justify-start items-center">
-  {/* Conteneur flex pour aligner le titre et la flèche */}
-  <div className="flex items-center mt-[-100] md:mt-6 lg:mt-4">
-    <h1 className="section-title leading-tight">Skills</h1>
-    
+        <div className="relative">
+
+
+          <h1 className=" absolute
+                          section-title
+                          z-10
+                          top-[-25%] sm:top-[-5%] md:top-[-5%] lg:top-[-5%]
+                          left-[30%] sm:left-[35%] md:left-[40%] lg:left-[30%]                          
+                          leading-tight
+                          text-shadow
+                          text-left"
+          >
+            Skills
     {/* Flèche */}
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor"
       className="section-arrow ml-16 text-colorChart mt-0 sm:mt-0 md:mt-0 lg:mt-0">
       <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18" />
     </svg>
-  </div>
+          </h1>
 
-          <div className="flex justify-center px-4 sm:px-8 md:px-12 lg:px-16">
-            <div className="paragraph-custom max-w-4xl w-full mx-auto relative top-[40px] md:top-[40px] lg:top-[50px] text-left justify-center">
-              <div className="grid grid-cols-1 sm:grid-cols-3 gap-8 pb-24 sm:pb-96 md:pb-96 lg:pb-96">
-                {/* Première colonne */}
-                <div className="text-center sm:text-left ">
-                  <strong className="text-[7vw] sm:text-xl md:text-2xl lg:text-3xl xl:text-2xl">Front end</strong>
-                  <div className="pt-6">
-                    <div className="skills-text flex justify-center sm:justify-between"><span>ReactJS</span></div>
-                    <div className="skills-text mt-2 flex justify-center sm:justify-between"><span>NextJS</span></div>
-                    <div className="skills-text mt-2 flex justify-center sm:justify-between"><span>Angular</span></div>
-                    <div className="skills-text mt-2 flex justify-center sm:justify-between"><span>Flutter</span></div>
-                    <div className="skills-text mt-2 flex justify-center sm:justify-between"><span>WordPress</span></div>
-                    <div className="skills-text mt-2 flex justify-center sm:justify-between"><span>Symfony</span></div>
-                    <div className="skills-text mt-2 flex justify-center sm:justify-between"><span>JavaScript</span></div>
-                    <div className="skills-text mt-2 flex justify-center sm:justify-between"><span>CSS</span></div>
-                    <div className="skills-text mt-2 flex justify-center sm:justify-between"><span>Tailwind CSS</span></div>
-                  </div>
-
-                  <div className="pt-6">
-                    <strong className="text-[6vw] sm:text-xl md:text-2xl lg:text-3xl xl:text-2xl">Prototypage & Design</strong>
-                    <div className="skills-text mt-2 flex justify-center sm:justify-between"><span>Figma</span></div>
-                    <div className="skills-text mt-2 flex justify-center sm:justify-between"><span>Balsamiq</span></div>
-                    <div className="skills-text mt-2 flex justify-center sm:justify-between"><span>Photoshop</span></div>
-                  </div>
-                </div>
-
-                {/* Deuxième colonne */}
-                <div className="text-center sm:text-left">
-                  <strong className="text-[7vw] sm:text-xl md:text-2xl lg:text-3xl xl:text-2xl">Back end</strong>
-                  <div className="pt-6">
-                    <div className="skills-text flex justify-center sm:justify-between"><span>NodeJS</span></div>
-                    <div className="skills-text mt-2 flex justify-center sm:justify-between"><span>TypeScript</span></div>
-                    <div className="skills-text mt-2 flex justify-center sm:justify-between"><span>Php</span></div>
-                    <div className="skills-text mt-2 flex justify-center sm:justify-between"><span>Java EE</span></div>
-                    <div className="skills-text mt-2 flex justify-center sm:justify-between"><span>PostgreSQL</span></div>
-                    <div className="skills-text mt-2 flex justify-center sm:justify-between"><span>MySQL</span></div>
-                    <div className="skills-text mt-2 flex justify-center sm:justify-between"><span>SQL Server</span></div>
-                    <div className="skills-text mt-2 flex justify-center sm:justify-between"><span>Sécurité Web et Web Mobile</span></div>
-                  </div>
-
-                  <div className="pt-6">
-                    <strong className="text-[6vw] sm:text-xl md:text-2xl lg:text-3xl xl:text-2xl">Modélisation & Conception</strong>
-                    <div className="skills-text mt-2 flex justify-center sm:justify-between"><span>UML (Unified Modeling Language)</span></div>
-                  </div>
-                </div>
-
-                {/* Troisième colonne */}
-                <div className="text-center sm:text-left">
-                  <strong className="text-[6.5vw] sm:text-xl md:text-2xl lg:text-3xl xl:text-2xl">Web Services & APIs</strong>
-                  <div className="pt-6">
-                    <div className="skills-text flex justify-center sm:justify-between"><span>Firebase</span></div>
-                    <div className="skills-text mt-2 flex justify-center sm:justify-between"><span>APIs REST</span></div>
-                  </div>
-
-                  <div className="pt-6">
-                    <strong className="text-[6.5vw] sm:text-xl md:text-2xl lg:text-3xl xl:text-2xl">Versioning</strong>
-                    <div className="skills-text mt-2 flex justify-center sm:justify-between"><span>GitHub</span></div>
-                  </div>
-
-                  {/* Lien vers la page Skills */}
-                  <div className="relative mt-8 mr-4 sm:mr-6 md:mr-8">
-                    <a href="/skills#skills"
-                      className="text-[#4d94ff] 
-                                hover:text-navLinkHover 
-                                font-medium 
-                                flex 
-                                items-center 
-                                justify-end 
-                                gap-1 
-                                group"
-                    >
-                      Niveaux de compétences estimés (Auto-évaluation)
-                      <span className=" transition-all 
-                                      group-hover:bg-yellow-500 
-                                      group-hover:text-white 
-                                      group-hover:rounded 
-                                      p-1
-                                      -ml-1"
-                      >
-                        ➡️
-                      </span>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+          {/* Paragraph */}
+          <p className="paragraph-custom
+                        w-full sm:w-full md:w-full lg:w-[65%]
+                        relative sm:relative md:relative lg:absolute
+                        center
+                        justify
+                        lg:top-[30%]
+                        left-[0%] sm:left-[0%] md:left-[0%] lg:left-[54%]                         
+                        px-12 sm:px-24 md:px-12
+                        mt-6 sm:mt-8 md:mt-12"
+          >
+            <span className=" mb-2
+                              block 
+                              break-words 
+                              whitespace-normal sm:whitespace-normal md:whitespace-normal 
+                              max-w-full"
+            >
+              Le développement logiciel : Une véritable <span className="text-[1.2rem] sm:text-[1.5rem]">vocation</span> qui m&apos;anime au quotidien !
+            </span>
+            <br />
+            Je m&apos;engage pleinement dans la conception d&apos;outils novateurs, performants et réellement utiles, pensés pour transformer et enrichir l&apos;expérience utilisateur. Mon objectif est de mettre la technologie au service de solutions concrètes, capables d&apos;améliorer la vie quotidienne.
+          </p>
 
         </div>
+        <DotsRectangle
+          className=" w-[40%] sm:w-[40%] md:w-[40%] lg:w-[20%]                    
+                      mt-8 sm:mt-8 md:mt-8 lg:mt-80
+                      self-center
+                      ml-auto"
+          rows={3}
+          dotsPerRow={10}
+          dotColor="#fff000"
+        />
+      </div>
 
       </div>
-    </div>
   );
 }
 
