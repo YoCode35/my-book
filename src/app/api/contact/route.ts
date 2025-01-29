d79eb694b7d29db846019c4b17ee6fde359405d9
YoCode35 <ygregoire35@gmail.com>
2025-01-29 14:02:45 +0100

    Cleaning comments code

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,26 +1,26 @@
 import nodemailer from 'nodemailer';
-import { NextRequest } from 'next/server'; // Importer le type NextRequest
+import { NextRequest } from 'next/server';
 
-export async function POST(req: NextRequest) {  // Définir explicitement le type de req
+export async function POST(req: NextRequest) {
   const { name, email, message } = await req.json();
 
   if (!name || !email || !message) {
     return new Response('Tous les champs sont obligatoires', { status: 400 });
   }
 
-  // Créer un transporteur Nodemailer
+  // Nodemailer
   const transporter = nodemailer.createTransport({
-    service: 'gmail', // Utiliser Gmail ou ton service d'email
+    service: 'gmail',
     auth: {
-      user: process.env.EMAIL_USER,  // Ton email (dans .env.local)
-      pass: process.env.EMAIL_PASS,  // Ton mot de passe ou mot de passe d'application
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
     },
   });
 
-  // Configurer l'email
+  //Email
   const mailOptions = {
-    from: email,  // Email de l'utilisateur
-    to: process.env.EMAIL_USER,  // Ton email
+    from: email,
+    to: process.env.EMAIL_USER,
     subject: `Message de ${name}`,
     text: `De: ${name} <${email}>\n\nMessage: ${message}`,
   };
@@ -28,8 +28,8 @@ export async function POST(req: NextRequest) {  // Définir explicitement le typ
   try {
     await transporter.sendMail(mailOptions);
     return new Response('Message envoyé avec succès', { status: 200 });
-  } catch (error: unknown) {  // Ajouter un type pour l'erreur
-    console.error(error);  // Afficher l'erreur dans la console pour le débogage
+  } catch (error: unknown) {
+    console.error(error);
     return new Response('Erreur lors de l\'envoi du message', { status: 500 });
   }
 }
