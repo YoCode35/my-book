dba9755ce40b9722d6e0c7bace130784752ceb0d
YoCode35 <ygregoire35@gmail.com>
2025-02-04 18:33:42 +0100

    Update Typo Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -18,8 +18,9 @@ export default function Footer() {
       <p className="relative 
                     z-10 
                     text-[#2d3748] 
-                    text-sm sm:text-base md:text-lg"
-      >© 2025 Mon Portfolio. Tous droits réservés.</p>
+                    text-sm sm:text-base md:text-lg
+                    font-orbitron"
+      >© 2025 Yoann GREGOIRE. Tous droits réservés.</p>
     </footer>
   );
 }
