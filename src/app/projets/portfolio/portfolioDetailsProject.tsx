666e852a073cce04eb5c83e2c4d94600d43f25b0
YoCode35 <ygregoire35@gmail.com>
2025-02-14 12:44:06 +0100

    Creation New project : Portfolio

---
+++ src/app/projets/portfolio/portfolioDetailsProject.tsx
@@ -0,0 +1,156 @@
+"use client";
+
+import Image from "next/image";
+import { PROJECT_NAMES } from "../../../components/navbar/route";
+
+export default function PortfolioDetailsProject() {
+
+  return (
+
+    // MyTodoList Section
+    <section id={PROJECT_NAMES.MYTODOLIST} className="project-section"
+      >
+        <h3 className="title-project">[Projet MyTodoList]</h3>
+
+        <a
+          href="https://www.mytodolist.tech"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={`link-project-portfolio`}
+        >
+          Web
+        </a>
+
+        <h4 className="project-description">Développement d’une application PWA (Progressive Web App) Cross Platform avec Flutter & PHP/MySQL.</h4>
+
+        <p className="unordered-list mb-12">
+          1. Planification et Conception
+          <br /><br />
+          - Objectifs du projet : Application multiplateforme (Mobile & Web)
+          <br />
+          - Besoins : Fonctionnalités principales et optimisation de l’expérience utilisateur
+          <br />
+          - Technique : Architecture de l’application, flux de données, infrastructure et déploiement
+          <br />
+          - UI/UX : Interface utilisateur intuitive et ergonomique
+          <br />
+          - Contraintes et risques : Optimisations spécifiques au développement multiplateforme, sécurité des données (RGPD), scalabilité
+        </p>
+
+        <p className="unordered-list mb-12">
+          2. Spécifications techniques et architecture de l’application
+          <br /><br />
+          - Front-end : Flutter (Progressive Web Application)
+          <br />
+          - Back-end : PHP/MySQL (APIs sécurisées via HTTPS)
+          <br />
+          <span className="block mb-6">- Développement en couches (IHM, Métier, Accès aux données, Persistance des données)</span>
+          <br />
+
+          <Image
+            src="/img/mytodolist/Architecture_Webapp.png"
+            alt="Architecture de l'application WebApp"
+            width={700}
+            height={809}
+            className="img_portfolio"
+            priority
+          />
+
+          <span className="block mb-6">- Base de données : MySQL avec une gestion relationnelle des utilisateurs et des tâches</span>
+
+          <Image
+            src="/img/mytodolist/mcd.png"
+            alt="Modèle conceptuel de données"
+            width={700}
+            height={875}
+            className="img_portfolio"
+            priority
+          />
+
+          <Image
+            src="/img/mytodolist/mld.png"
+            alt="Modèle logique de données"
+            width={700}
+            height={489}
+            className="img_portfolio"
+            priority
+          />
+
+          <Image
+            src="/img/mytodolist/mpd.png"
+            alt="Modèle physique de données"
+            width={700}
+            height={563}
+            className="img_portfolio"
+            priority
+          />
+
+          <br />
+          - Déploiement : Hébergement mutualisé, serveur web (Apache/Nginx), certificats SSL/TLS
+        </p>
+
+        <p className="unordered-list mb-12">
+          3. Développement Back-End
+          <br /><br />
+          - Utilisateur : Inscription/connexion (+ Firebase Authentication)
+          <br />
+          - Compte : Gestion de compte, réinitialisation sécurisée e.mail/mot de passe
+          <br />
+          - Tâches/Listes : API CRUD (Create, Read, Update, Delete)
+          <br />
+          - Notifications : Inscription, mise à jour e.mail, réinitialisation mot de passe
+          <br />
+          - Sécurité
+        </p>
+
+        <p className="unordered-list mb-12">
+          4. Développement Front-End
+          <br /><br />
+          - UI/UX (Prototypage) : Balsamiq et Figma
+          <br />
+          - Infographie : Bitmap logo/illustration
+          <br />
+          - Barre de navigation et menu contextuel
+          <br />
+          - Écran d’accueil
+          <br />
+          - Pages annexes
+          <br />
+          - Écrans d’inscription/connexion
+          <br />
+          - Tâches et listes
+          <br />
+          - Sécurité
+        </p>
+
+        <p className="unordered-list mb-12">
+          5. Sécurité
+          <br /><br />
+          - Gestion des rôles
+          <br />
+          - Connexions sécurisées via HTTPS
+          <br />
+          - Sessions sécurisées
+          <br />
+          - Mise en place de mécanismes contre les attaques courantes (XSS, injections SQL, CSRF, anti-bruteforce)
+          <br />
+          - Cryptage des mots de passes
+        </p>
+
+        <p className="unordered-list mb-12">
+          6. Tests
+          <br /><br />
+          - Tests unitaires
+          <br />
+          - Tests fonctionnels
+          <br />
+          - Tests de performance
+          <br />
+          - Tests de compatibilité
+          <br />
+          - Tests utilisateurs
+        </p>
+
+      </section>
+  );
+}
---
+++ src/app/projets/portfolio/portfolioProject.tsx
@@ -0,0 +1,48 @@
+import ProjectCard from "../../../components/projects/ProjectsCard";
+import DotsRectangle from "../../../components/dots/DotsRectangle";
+import { ROUTES } from "../../../components/navbar/route";
+
+const PortfolioProject = () => {
+  return (
+    <section className="section-container">
+      <ProjectCard
+        title="MyTodoList"
+        description=""
+        date="Octobre 2024"
+        technologies="Flutter & MySQL"
+        imageUrl="/img/mytodolist/ref_mytodolist.png"
+        imageAlt="Image de mon projet d'application MyTodoList"
+        projectLink="https://www.mytodolist.tech"
+        moreInfoLink={ROUTES.PROJECTS.MYTODOLIST}
+        linkLabel="Web"
+        linkClassName={"link-project-home"}
+        imageWidth={789}
+        imageHeight={360}
+      />
+
+      {/* DotsRectangle */}
+      <div
+        className=" xl:w-full sm:w-[40%] lg:w-[100%] 
+                    xl:flex 
+                    xl:justify-start 
+                    w-full 
+                    max-w-[320px] 
+                    mt-8 xl:mt-10 2xl:mt-20 
+                    relative 
+                    mx-auto 
+                    xl:ml-48 4xl:ml-[800px]"
+      >
+        <DotsRectangle
+          className="w-full"
+          rows={3}
+          dotsPerRow={10}
+          dotColor="#fff000"
+          width="100%"
+          height="45px"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default PortfolioProject;
