c83ae82d31fbb2082258ca3a74c9d08890951921
YoCode35 <ygregoire35@gmail.com>
2025-02-14 15:14:24 +0100

    Update Portfolio project v3

--- src/app/projets/portfolio/portfolioDetailsProject.tsx
+++ src/app/projets/portfolio/portfolioDetailsProject.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Image from "next/image";
+//import Image from "next/image";
 import { PROJECT_NAMES } from "../../../components/navbar/route";
 
 export default function PortfolioDetailsProject() {
@@ -8,12 +8,12 @@ export default function PortfolioDetailsProject() {
   return (
 
     // MyTodoList Section
-    <section id={PROJECT_NAMES.MYTODOLIST} className="project-section"
+    <section id={PROJECT_NAMES.PORTFOLIO} className="project-section"
       >
-        <h3 className="title-project">[Projet MyTodoList]</h3>
+        <h3 className="title-project">[Projet Portfolio]</h3>
 
         <a
-          href="https://www.mytodolist.tech"
+          href="https://my-book-black.vercel.app/"
           target="_blank"
           rel="noopener noreferrer"
           className={`link-project-portfolio`}
@@ -21,135 +21,63 @@ export default function PortfolioDetailsProject() {
           Web
         </a>
 
-        <h4 className="project-description">Développement d’une application PWA (Progressive Web App) Cross Platform avec Flutter & PHP/MySQL.</h4>
+        <h4 className="project-description">Développement d’un Site Web avec React.js, Next.js, Node.js, Express, TypeScript, PostGreSQL, Tailwind.</h4>
 
-        <p className="unordered-list mb-12">
-          1. Planification et Conception
-          <br /><br />
-          - Objectifs du projet : Application multiplateforme (Mobile & Web)
-          <br />
-          - Besoins : Fonctionnalités principales et optimisation de l’expérience utilisateur
-          <br />
-          - Technique : Architecture de l’application, flux de données, infrastructure et déploiement
-          <br />
-          - UI/UX : Interface utilisateur intuitive et ergonomique
-          <br />
-          - Contraintes et risques : Optimisations spécifiques au développement multiplateforme, sécurité des données (RGPD), scalabilité
+        <p className="unordered-list text-white mb-6">
+          <span className="text-colorChart underline block mb-2">Résumé du projet</span>
+
         </p>
 
-        <p className="unordered-list mb-12">
-          2. Spécifications techniques et architecture de l’application
-          <br /><br />
-          - Front-end : Flutter (Progressive Web Application)
-          <br />
-          - Back-end : PHP/MySQL (APIs sécurisées via HTTPS)
-          <br />
-          <span className="block mb-6">- Développement en couches (IHM, Métier, Accès aux données, Persistance des données)</span>
-          <br />
+        <div className="unordered-list text-white mb-6">
+          <span className="text-colorChart underline block mb-2">Technologies utilisées</span>
+          <ul className="ml-0 list-none">
+            <li>- <strong>Back-end :</strong> </li>
+            <li>- <strong>Base de données :</strong> </li>
+            <li>- <strong>Administration des données :</strong> </li>
+            <li>- <strong>Langages principaux :</strong> </li>
+            <li>- <strong>Mise en ligne et tests :</strong> </li>
+          </ul>
+        </div>
 
-          <Image
-            src="/img/mytodolist/Architecture_Webapp.png"
-            alt="Architecture de l'application WebApp"
-            width={700}
-            height={809}
-            className="img_portfolio"
-            priority
-          />
 
-          <span className="block mb-6">- Base de données : MySQL avec une gestion relationnelle des utilisateurs et des tâches</span>
+        <div className="unordered-list text-white mb-6">
+          <span className="text-colorChart underline block mb-2">Fonctionnalités principales</span>
 
-          <Image
-            src="/img/mytodolist/mcd.png"
-            alt="Modèle conceptuel de données"
-            width={700}
-            height={875}
-            className="img_portfolio"
-            priority
-          />
+          <ul className="ml-0 list-none">
+            <li><strong>1. </strong>
+              <ul className="ml-2 list-none">
+                <li>- </li>
+                <li>- </li>
+                <ul className="ml-12 list-disc">
+                  <li></li>
+                  <li></li>
+                </ul>
+              </ul>
+            </li>
 
-          <Image
-            src="/img/mytodolist/mld.png"
-            alt="Modèle logique de données"
-            width={700}
-            height={489}
-            className="img_portfolio"
-            priority
-          />
+            <li className="mt-6"><strong>2. </strong>
+              <ul className="ml-2 list-none">
+                <li>- </li>
+                <li>- </li>
+              </ul>
+            </li>
 
-          <Image
-            src="/img/mytodolist/mpd.png"
-            alt="Modèle physique de données"
-            width={700}
-            height={563}
-            className="img_portfolio"
-            priority
-          />
+            <li className="mt-6"><strong>3. </strong>
+              <ul className="ml-2 list-none">
+                <li>- </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
 
-          <br />
-          - Déploiement : Hébergement mutualisé, serveur web (Apache/Nginx), certificats SSL/TLS
-        </p>
-
-        <p className="unordered-list mb-12">
-          3. Développement Back-End
-          <br /><br />
-          - Utilisateur : Inscription/connexion (+ Firebase Authentication)
-          <br />
-          - Compte : Gestion de compte, réinitialisation sécurisée e.mail/mot de passe
-          <br />
-          - Tâches/Listes : API CRUD (Create, Read, Update, Delete)
-          <br />
-          - Notifications : Inscription, mise à jour e.mail, réinitialisation mot de passe
-          <br />
-          - Sécurité
-        </p>
-
-        <p className="unordered-list mb-12">
-          4. Développement Front-End
-          <br /><br />
-          - UI/UX (Prototypage) : Balsamiq et Figma
-          <br />
-          - Infographie : Bitmap logo/illustration
-          <br />
-          - Barre de navigation et menu contextuel
-          <br />
-          - Écran d’accueil
-          <br />
-          - Pages annexes
-          <br />
-          - Écrans d’inscription/connexion
-          <br />
-          - Tâches et listes
-          <br />
-          - Sécurité
-        </p>
-
-        <p className="unordered-list mb-12">
-          5. Sécurité
-          <br /><br />
-          - Gestion des rôles
-          <br />
-          - Connexions sécurisées via HTTPS
-          <br />
-          - Sessions sécurisées
-          <br />
-          - Mise en place de mécanismes contre les attaques courantes (XSS, injections SQL, CSRF, anti-bruteforce)
-          <br />
-          - Cryptage des mots de passes
-        </p>
-
-        <p className="unordered-list mb-12">
-          6. Tests
-          <br /><br />
-          - Tests unitaires
-          <br />
-          - Tests fonctionnels
-          <br />
-          - Tests de performance
-          <br />
-          - Tests de compatibilité
-          <br />
-          - Tests utilisateurs
-        </p>
+        <div className="unordered-list text-white mb-8">
+          <span className="text-colorChart underline block mb-2">Points forts techniques</span>
+          <ul className="ml-0 list-none">
+            <li>- <strong>Respect des bonnes pratiques de développement :</strong> </li>
+            <li>- <strong>Structure modulaire :</strong> </li>
+            <li>- <strong>Performances :</strong> </li>
+          </ul>
+        </div>
 
       </section>
   );
