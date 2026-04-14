151a8008ddb6fdb35c217e2f985fbf5d6f06d5c5
YoannG <ygregoire35@gmail.com>
2026-04-14 15:30:35 +0200

    Update skills detail

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -30,22 +30,18 @@ export default function Skills() {
             >
                 <p className="paragraph-custom mt-4">
                     J’exerce dans le secteur du numérique depuis une vingtaine d’années. J’ai commencé par maitriser le développement front-end, avec l’HTML, le CSS, le JavaScript et quelques notions en PHP.
-                    Au gré de mes développements, je me suis toujours efforcé à factoriser mon code et à l’écrire de la manière la plus propre possible.
+                    Au gré de mes développements, je me suis toujours efforcé de factoriser mon code et à l’écrire de la manière la plus propre possible.
                 </p>
                 <p className="paragraph-custom mt-4">
-                    Ma montée en compétences récente en back-end me permet aujourd’hui de développer une application from scratch jusqu’à son aboutissement.
-                </p>
-                <p className="paragraph-custom mt-4">
-                    J’exerce actuellement ces compétences en me perfectionnant et en explorant des stacks modernes comme React.js, Next.js, TypeScript, Node.js ou encore Flutter.
+                    
                 </p>
                 <div className="paragraph-custom mt-4">
-                    <p>Pour tous mes développements, je souhaite :</p>
+                    <p>J’exerce actuellement ces compétences sur des stacks modernes et pour tous mes développements :</p>
                     <ul className="mt-2">
-                        <li>- Exploiter les patterns de conception actuels</li>
-                        <li>- Intégrer des tests automatisés dans mon workflow</li>
-                        <li>- Mettre en place un système de CI/CD robuste</li>
-                        <li>- Utiliser des outils de build modernes</li>
-                        <li>- Continuer à utiliser TypeScript</li>
+                        <li>- J’Exploite les patterns de conception actuels</li>
+                        <li>- J’Intégre des tests automatisés dans mon workflow</li>
+                        <li>- Je Mets en place un système de CI/CD robuste</li>
+                        <li>- J’Utilise des outils de build modernes</li>
                     </ul>
                 </div>
             </div>
