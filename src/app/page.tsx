ff7f8d2b11a3d018cae9059d84e8e835eb376278
YoannG <ygregoire35@gmail.com>
2026-04-14 15:08:50 +0200

    Update skills section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -224,6 +224,7 @@ export default function Home() {
               <div className="skills-text"><span>JavaScript</span></div>
               <div className="skills-text"><span>Flutter</span></div>
               <div className="skills-text"><span>React.js</span></div>
+              <div className="skills-text"><span>Angular.js</span></div>
               <div className="skills-text"><span>Next.js</span></div>
               <div className="skills-text"><span>TypeScript</span></div>
               <div className="skills-text"><span>HTML</span></div>
